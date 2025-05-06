81c9d596bbe976d58b0c8204aed64b1768a645ef
kylekimler <kylejkimler@gmail.com>
2025-05-06 19:22:28 -0400

    Remove Rcheck and BiocCheck artifacts; update .gitignore

--- ..Rcheck/tests/testthat/test_mockLong.R
+++
@@ -1,5 +0,0 @@
-test_that("mockLong returns correct structure", {
-    x <- mockLong()
-    expect_true(is.data.frame(x))
-    expect_true(all(c("bc", "type", "sample", "batch") %in% colnames(x)))
-})
